85fef9bdeb0aedfb6b04508b2f859f2a2878e005
Richard Dubný <72037908+R1xhark@users.noreply.github.com>
2021-11-17 21:51:22 +0100

    Update Class1.cs

--- BITOVAMAPA/Class1.cs
+++ BITOVAMAPA/Class1.cs
@@ -8,28 +8,26 @@ using System.Windows.Forms;
 namespace BITOVAMAPA
 {
     
-        // Deklarace třídy, jejíž instance dovedou pracovat s bitovými mapami.
+        
         public class BitovaMapa
         {
             // Definice soukromých datových členů třídy.
             private string cestaKBitmape;
             private Bitmap bitmapa;
-            // Definice veřejného instančního bezparametrického konstruktoru.
+            
             public BitovaMapa()
             {
             cestaKBitmape = Environment.CurrentDirectory + @"C:\Users\Richard\Downloads\jeri.bmp";
                 NacistBitovouMapu(cestaKBitmape);
             }
-            // Definice veřejné instanční parametrické metody, která provede
-            // načtení bitové mapy z fyzického souboru. 
+         
             
  public void NacistBitovouMapu(string cesta)
             {
                 cestaKBitmape = cesta;
                 bitmapa = new Bitmap(cesta);
             }
-            // Definice veřejné instanční metody, která zobrazí bitovou mapu
-            // na formuláři.
+            
             public void ZobrazitBitovouMapu()
             {
                 Form formular = Form.ActiveForm;
